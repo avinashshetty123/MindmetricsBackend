8cafe42c5e6391330de61354a097909ca4146cdc
Avinash <abhiapril122005@gmail.com>
2025-03-31 18:28:31 +0530

    add debugging log for user information in session route

--- routes.js
+++ routes.js
@@ -47,6 +47,7 @@ router.get("/auth/session", (req, res) => {
   console.log("Session Data:", req.session);
   console.log("Authenticated:", req.isAuthenticated());// Debugging line
   if (req.isAuthenticated()) {
+    console.log("User:", req.user); // Debugging line
     res.json({ authenticated: true, user: req.user });
   } else {
     res.json({ authenticated: false });
