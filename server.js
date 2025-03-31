a8701573ad8362b9cdb6a7d90c09ad2d7a672016
Avinash <abhiapril122005@gmail.com>
2025-03-31 13:28:35 +0530

    remove unused path module and static file serving for React app

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";
+
 import setupAuth from "./auth.js";
 import routes from "./routes.js";
 
@@ -20,13 +20,7 @@ setupAuth(app);
 app.use("/", routes);
 
 // Serve React build folder
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "dist")));
 
-// Serve React app for all routes (React handles routing)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
 
 // Start Server
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
