e320a76f8a9eea3c71eace8b82d1225143825ea3
Avinash <abhiapril122005@gmail.com>
2025-03-31 13:55:42 +0530

    refactor server.js to simplify middleware and update response messages

--- server.js
+++ server.js
@@ -1,32 +1,14 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-
-import setupAuth from "./auth.js";
-import routes from "./routes.js";
+import express from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-app.use(express.json());
-
-// Setup Authentication
-setupAuth(app);
-
-// Use API Routes
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Server is running!');
+  res.send('MindMetrics Backend is running!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
-// Serve React build folder
-
-
-// Start Server
-
