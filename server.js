dc320f5a197cf1e916d1d27d1f6dccd4db72ab32
Avinash <abhiapril122005@gmail.com>
2025-03-31 14:33:23 +0530

    update CORS configuration to allow requests from Netlify deployment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://mindmetricss.netlify.app", credentials: true }));
 app.use(express.json());
 
 // Setup Authentication
